c9c87122ca8e3341df896b708e29d1837588fee5
ppnieto <ppnieto@gmail.com>
2015-10-30 19:37:50 +0100

    refactor

--- app/http/mainController.php
+++ app/http/mainController.php
@@ -1,6 +1,7 @@
 ﻿<?php
 
 Moriarti::register(1,'/http/*',function($tipo,$data) {
+	ini_set('display_errors', 1);
 	if (isset($_SESSION['USER'])) {
 		Moriarti::store('layout','private');
 	} else {
--- include/sendDeferred.php
+++ include/sendDeferred.php
@@ -2,13 +2,9 @@
 	require_once(dirname(__FILE__) . "/../classes/Autoloader.php");		
 	
 	$seconds = $argv[1];
-	$tipo = $argv[2];	
-	
-	echo "$seconds $tipo \n";
-	
+	$code = $argv[2];	
+
 	sleep($seconds);
-	echo "$seconds $tipo ok\n";
-	\Eventos::send($tipo);	
-	echo "$seconds $tipo ok 2\n";
+	new Message($code);
 
 ?>
